02f1fc902f80845e11c1a5cafbe9291e309959d1
Wesley Duarte <wesleydmuniz@outlook.com>
2021-03-26 17:45:55 -0300

    feat: 🎸 display cat gifs

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,34 @@
 import Head from 'next/head'
+import { useGiphySearch } from './useGiphySearch';
 
-export default function Home() {
+export default function Home(initialData) {
+
   return (
-    <div>
+    <div className='container'>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/styles.css"/>
       </Head>
       <h1>Gyphy Search App</h1>
+
+      <div class="giphy-search-results-grid">
+        {initialData.catGifs.data.map((each, index) => {
+          return(
+            <div key="index">
+              <h3>{each.title}</h3>
+              <img src={each.images.original.url} alt={each.title}/>
+            </div>
+          )
+        })}
+      </div>
     </div>
     
   )
+}
+
+export async function getStaticProps() {
+  const response = await useGiphySearch().getGifs('cats')
+  const catGifs = await response.json()
+  return { props: { catGifs : catGifs } }
 }
